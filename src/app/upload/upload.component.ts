924709e43e4bca5719e8b2d98d15814648572979
Jesse Hyde <jesse@custombased.com>
2020-09-15 09:52:57 +1200

    Image not displays within same component after upload

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -11,6 +11,8 @@ export class UploadComponent implements OnInit {
 
   error: string;
   dragAreaClass: string;
+  imgDisplayAreaClass: string;
+  imgURL: any;
   
   onFileChange(event: any) {
     let files: FileList = event.target.files;
@@ -23,26 +25,31 @@ export class UploadComponent implements OnInit {
 
   @HostListener("dragover", ["$event"]) onDragOver(event: any) {
     this.dragAreaClass = "droparea";
+    console.log("DragOver");
     event.preventDefault();
   }
 
   @HostListener("dragenter", ["$event"]) onDragEnter(event: any) {
     this.dragAreaClass = "droparea";
+    console.log("DragEnter");
     event.preventDefault();
   }
 
   @HostListener("dragend", ["$event"]) onDragEnd(event: any) {
     this.dragAreaClass = "dragarea";
+    console.log("DragEnd");
     event.preventDefault();
   }
 
   @HostListener("dragleave", ["$event"]) onDragLeave(event: any) {
     this.dragAreaClass = "dragarea";
+    console.log("DragLeave");
     event.preventDefault();
   }
-  
+
   @HostListener("drop", ["$event"]) onDrop(event: any) {
     this.dragAreaClass = "dragarea";
+    console.log("Drop");
     event.preventDefault();
     event.stopPropagation();
     if (event.dataTransfer.files) {
@@ -53,11 +60,29 @@ export class UploadComponent implements OnInit {
 
   saveFiles(files: FileList) {
 
-    if (files.length > 1) this.error = "Only one file at time allow";
-    else {
-      this.error = "";
-      console.log(files[0].size,files[0].name,files[0].type);
+    if (files.length > 1) {
+      this.error = "Only one file at time allow";
     }
+
+    if (files[0].type != "image/png") {
+      this.error = "File must be PNG";
+    }
+
+    if (files[0].type == "image/png") {
+
+      this.imgDisplayAreaClass = "imgDisplayArea";
+      
+      var reader = new FileReader();
+
+      reader.readAsDataURL(files[0]); 
+
+      reader.onload = (_event) => { 
+        this.imgURL = reader.result; 
+      }
+
+    }
+
   }
+
 
 }
